ca634a76c3aba1174fc3b4c672d970f5afd655d1
sujain <sujain@roku.com>
2021-10-26 16:30:36 -0500

    commoon utils

---
+++ E2EAF_common/common_utils.py
@@ -0,0 +1,10 @@
+import datetime
+from time import sleep
+
+
+def get_random_string():
+    sleep(1)
+    return str(datetime.datetime.now())\
+        .replace("-", "")\
+        .replace(" ", "")\
+        .replace(":", "")
