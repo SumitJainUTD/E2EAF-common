51a2cf6e94d2e4d81aaa1fcd227a2cf1d533ec98
SumitJainUTD <sumi12june@gmail.com>
2021-10-25 23:23:27 -0500

    update

--- apps-common/__init__.py
+++ apps-common/__init__.py
@@ -1,4 +1,4 @@
 from .api_client import ApiClient
 from .exceptions import DataError
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
