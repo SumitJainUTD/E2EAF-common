31e26f4239e79dee1a57df2f74edfb1dd8f05e9e
sujain <sujain@roku.com>
2021-10-26 15:30:24 -0500

    bug fix

--- E2EAF_common/api_client.py
+++ E2EAF_common/api_client.py
@@ -3,63 +3,55 @@ import requests
 
 class ApiClient(object):
 
-    def call_api(self, url, method, query_params=None, headers=None,
-                 post_params=None, body=None, _preload_content=True,
-                 _request_timeout=None):
+    def call_api(self, method, url,
+            params=None, data=None, headers=None, cookies=None, files=None,
+            auth=None, timeout=None, allow_redirects=True, proxies=None,
+            hooks=None, stream=None, verify=None, cert=None, json=None):
         """Makes the HTTP request using RESTClient."""
         if method == "GET":
             return requests.get(url,
-                                params=query_params,
-                                _preload_content=_preload_content,
-                                _request_timeout=_request_timeout,
+                                params=params,
+                                timeout=timeout,
                                 headers=headers)
 
         elif method == "HEAD":
             return requests.head(url,
-                                 params=query_params,
-                                 _preload_content=_preload_content,
-                                 _request_timeout=_request_timeout,
+                                 params=params,
+                                 timeout=timeout,
                                  headers=headers)
 
         elif method == "OPTIONS":
             return requests.options(url,
-                                    params=query_params,
+                                    params=params,
                                     headers=headers,
-                                    post_params=post_params,
-                                    _preload_content=_preload_content,
-                                    _request_timeout=_request_timeout,
-                                    body=body)
+                                    timeout=timeout,
+                                    data=data)
         elif method == "POST":
             return requests.post(url,
-                                 params=query_params,
+                                 params=params,
                                  headers=headers,
-                                 post_params=post_params,
-                                 _preload_content=_preload_content,
-                                 _request_timeout=_request_timeout,
-                                 body=body)
+                                 timeout=timeout,
+                                 data=data)
         elif method == "PUT":
             return requests.put(url,
-                                params=query_params,
+                                params=params,
                                 headers=headers,
-                                post_params=post_params,
-                                _preload_content=_preload_content,
-                                _request_timeout=_request_timeout,
-                                body=body)
+                                timeout=timeout,
+                                data=data)
         elif method == "PATCH":
             return requests.patch(url,
-                                  params=query_params,
+                                  params=params,
                                   headers=headers,
-                                  post_params=post_params,
-                                  _preload_content=_preload_content,
-                                  _request_timeout=_request_timeout,
-                                  body=body)
+
+
+                                  timeout=timeout,
+                                  data=data)
         elif method == "DELETE":
             return requests.delete(url,
-                                   params=query_params,
+                                   params=params,
                                    headers=headers,
-                                   _preload_content=_preload_content,
-                                   _request_timeout=_request_timeout,
-                                   body=body)
+                                   timeout=timeout,
+                                   data=data)
         else:
             raise ValueError(
                 "http method must be `GET`, `HEAD`, `OPTIONS`,"
