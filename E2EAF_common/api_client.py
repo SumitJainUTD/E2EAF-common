b5dbdfb57ef4e315af19372990947224cf1d9f62
sujain <sujain@roku.com>
2021-10-26 15:14:03 -0500

    bug fix

--- E2EAF_common/api_client.py
+++ E2EAF_common/api_client.py
@@ -3,27 +3,27 @@ import requests
 
 class ApiClient(object):
 
-    def call_api(self, url, method, data, query_params=None, headers=None,
+    def call_api(self, url, method, query_params=None, headers=None,
                  post_params=None, body=None, _preload_content=True,
                  _request_timeout=None):
         """Makes the HTTP request using RESTClient."""
         if method == "GET":
             return requests.get(url,
-                                query_params=query_params,
+                                params=query_params,
                                 _preload_content=_preload_content,
                                 _request_timeout=_request_timeout,
                                 headers=headers)
 
         elif method == "HEAD":
             return requests.head(url,
-                                 query_params=query_params,
+                                 params=query_params,
                                  _preload_content=_preload_content,
                                  _request_timeout=_request_timeout,
                                  headers=headers)
 
         elif method == "OPTIONS":
             return requests.options(url,
-                                    query_params=query_params,
+                                    params=query_params,
                                     headers=headers,
                                     post_params=post_params,
                                     _preload_content=_preload_content,
@@ -31,7 +31,7 @@ class ApiClient(object):
                                     body=body)
         elif method == "POST":
             return requests.post(url,
-                                 query_params=query_params,
+                                 params=query_params,
                                  headers=headers,
                                  post_params=post_params,
                                  _preload_content=_preload_content,
@@ -39,7 +39,7 @@ class ApiClient(object):
                                  body=body)
         elif method == "PUT":
             return requests.put(url,
-                                query_params=query_params,
+                                params=query_params,
                                 headers=headers,
                                 post_params=post_params,
                                 _preload_content=_preload_content,
@@ -47,7 +47,7 @@ class ApiClient(object):
                                 body=body)
         elif method == "PATCH":
             return requests.patch(url,
-                                  query_params=query_params,
+                                  params=query_params,
                                   headers=headers,
                                   post_params=post_params,
                                   _preload_content=_preload_content,
@@ -55,7 +55,7 @@ class ApiClient(object):
                                   body=body)
         elif method == "DELETE":
             return requests.delete(url,
-                                   query_params=query_params,
+                                   params=query_params,
                                    headers=headers,
                                    _preload_content=_preload_content,
                                    _request_timeout=_request_timeout,
